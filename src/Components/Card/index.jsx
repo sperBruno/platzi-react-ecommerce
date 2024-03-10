d4840de1a8c350c2f36d3667b991d9c77a5fe98e
Bruno Barrios <sperbru797@gmail.com>
2024-03-09 22:53:16 -0400

    🚧 fetch data

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,20 +1,25 @@
-const Card = () => {
-   return (
+const Card = (item) => {
+  return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
-        <figure className="relative mb-2 w-full h-4/5">
-            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-                Electronics
-            </span>
-            <img className="w-full h-full object-cover rounded-lg"
-             src="https://media.istockphoto.com/id/860853774/es/foto/auriculares-azul-aislado-sobre-un-fondo-blanco.jpg?s=612x612&w=is&k=20&c=8BcnjTjhdula0Wbhe_q8JICMjmxUV0Sm_ILy-Y-ZDn4=" alt="hadphones"></img>
-            <div className="absolute top-0 right-0 flex justify-center items-center bg-gray-50 w-6 h-6 rounded-full text-center text-medium m-2 p-1">+</div>
-        </figure>
-        <p className="flex justify-between">
-            <span className="text-sm font-light">Headphones</span>
-            <span  className="text-lg font-medium">$300</span>
-        </p>
+      <figure className="relative mb-2 w-full h-4/5">
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          {item.item.category.name}
+        </span>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={item.item.images[0]}
+          alt={item.item.title}
+        ></img>
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-gray-50 w-6 h-6 rounded-full text-center text-medium m-2 p-1">
+          +
+        </div>
+      </figure>
+      <p className="flex justify-between">
+        <span className="text-sm font-light">{item.item.title}</span>
+        <span className="text-lg font-medium">${item.item.price}</span>
+      </p>
     </div>
-   ) 
-}
+  );
+};
 
 export default Card;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,18 +1,28 @@
+import { useState, useEffect } from "react";
 import Card from "../../Components/Card";
 import Layout from "../../Components/Layout";
 
 function Home() {
+  const [items, setItems] = useState(null);
 
-    return (
-        <Layout>
-            Home
-            <Card >
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
 
-            </Card>
-        </Layout>
-          
-    )
-  }
-  
-  export default Home
-  
+  return (
+    <Layout>
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => {
+          console.log(JSON.stringify(item.images[0]));
+
+          return <Card item={item} key={item.id}></Card>;
+        })}
+      </div>
+    </Layout>
+  );
+}
+
+export default Home;
